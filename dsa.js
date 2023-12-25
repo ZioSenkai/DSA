906f30cdf187a4575e9093145294eff6d42cc8e1
Zio <143772055+ZioSenkai@users.noreply.github.com>
2023-12-25 22:23:53 +0700

    Add files via upload

---
+++ dsa.js
@@ -0,0 +1,26 @@
+// Bài 1:
+
+console.log('Hello World');
+alert('Hello World');
+
+// Bài 2:
+
+const hello = 'Hello World';
+
+console.log(hello);
+
+// Bài 3:
+
+const a = 'Hello';
+const b = 'World';
+const c = a + " " + b;
+
+console.log(c);
+
+// Bài 4:
+
+function print() {
+  return c;
+}
+
+console.log(print());
