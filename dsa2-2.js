8f2c5ef106cb31255b59ec7b919bdc2a51464f60
Zio <143772055+ZioSenkai@users.noreply.github.com>
2024-01-04 12:43:41 +0700

    Add files via upload

---
+++ dsa2-2.js
@@ -0,0 +1,10 @@
+class Student {
+  constructor(name, age, className) {
+    this.name = name;
+    this.age = age;
+    this.className = className;
+  }
+}
+
+const newStd = new Student ('Nguyễn Văn A', 6, '1A7')
+console.log(newStd);
